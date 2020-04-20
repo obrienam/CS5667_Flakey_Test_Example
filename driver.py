c297b106047f48f3da937f7503409b0858348509
Aidan O'Brien <obrienam@appstate.edu>
2020-04-20 14:56:16 -0400

    Finished up demo test

---
+++ driver.py
@@ -0,0 +1,12 @@
+import random
+class driver:
+    x=[1,2,3,4,5]
+    random.shuffle(x)
+    def getOutput(self):
+        num=random.choice(self.x)
+        self.x.remove(num)
+        return num
+if __name__=='__main__':
+    test=driver()
+    for i in range(5):
+        print(test.getOutput())
---
+++ test.py
@@ -0,0 +1,18 @@
+import unittest
+import driver as driverClass
+class Testing(unittest.TestCase):
+    def test_flake(self):
+        c=4
+        test=driverClass.driver()
+        o=test.getOutput()
+        self.assertEqual(c,o)
+    def test_notflake(self):
+        c=4
+        test=driverClass.driver()
+        for i in range(5):
+            o=test.getOutput()
+            if(o==4):
+                break
+        self.assertEqual(c,o)
+if __name__ == '__main__':
+    unittest.main()
